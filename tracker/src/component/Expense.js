4ddc9477425cd327a3a65cb4e92cba2d71e03d01
PALLAB GHOSH <gpallab405@gmail.com>
2023-04-25 12:28:02 +0530

    Give structure the Expense file
    Add a structure which return the UI of Income Expense Remaining balance and add classNaem to the div for adding css

--- tracker/src/component/Expense.js
+++ tracker/src/component/Expense.js
@@ -2,6 +2,27 @@ import React from 'react'
 
 export default function Expense() {
   return (
-    <div>Expense</div>
+    <div>
+    <div className='blc'>
+    <h2>Your Balance</h2>
+    <div className='balance-val'>{income-expense}/-</div>
+ </div>
+
+ <div className='row row-expense'>
+ 
+   <div className='col col-income'>
+   <span>
+   <h3>Income</h3>
+     <div className='income-text'>{income}/-</div>
+   </span>
+    </div>
+
+   <div  className='col col-expense'>
+     <h3 >Expense</h3>
+     <div className='expense-text'>{expense}/-</div>
+   </div>
+
+ </div>
+    </div>
   )
 }
