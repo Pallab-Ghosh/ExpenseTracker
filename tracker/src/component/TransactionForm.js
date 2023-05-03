5de7f016ffec9b0c002139321916a4549564d293
PALLAB GHOSH <gpallab405@gmail.com>
2023-05-03 18:22:20 +0530

    create the form of  new transaction

--- tracker/src/component/TransactionForm.js
+++ tracker/src/component/TransactionForm.js
@@ -4,9 +4,28 @@ export const TransactionForm = () => {
   const [name,setname]=useState('')
   const [amt,setamt]=useState('')
   return (
+    <div className='form1'>
+    <h2>Add new Transaction</h2>
+    <form className='transaction-form'>
+     <label>
+        Name
+        <div>
+           <input type='text' value ={name}  placeholder='Enter-amount_name' onChange={(e)=>setname(e.target.value)}/>
+        </div>
+      </label>
+
+      <label>
+        Amount
+        <div>
+            <input type='number' value={amt} placeholder='Enter-amount'  onChange={(e)=>setamt(e.target.value)}/>
+        </div>
+      </label>
     <div>
-    
-    
+        <button  className= "income-btn"  onClick={(e)=>handle('income',e)}>Add Income</button>
+        <button  className="expense-btn" onClick={(e)=>handle('expense',e)}>Add Expense</button>
+     
+    </div>
+ </form>
     
     </div>
   )
