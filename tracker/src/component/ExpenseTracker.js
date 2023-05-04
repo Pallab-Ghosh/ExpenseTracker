284fb3910b4b092de702b3860058d8730be09df6
PALLAB GHOSH <gpallab405@gmail.com>
2023-05-04 22:54:21 +0530

    create a transactiondata
    initially set the state of income,expense,transaction

--- tracker/src/component/ExpenseTracker.js
+++ tracker/src/component/ExpenseTracker.js
@@ -1,6 +1,11 @@
 import React from 'react'
 
+
+const transactionData=[]
 export const ExpenseTracker = () => {
+  const [income,setincome]=useState(0)
+  const[expense,setexpense]=useState(0)
+  const[transactions,settransactions]=useState(transactionData)
   return (
     <div>ExpenseTracker</div>
   )
