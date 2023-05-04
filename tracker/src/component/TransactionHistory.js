1cea397acb4e1e2c65aead7551b684e01d1348e4
PALLAB GHOSH <gpallab405@gmail.com>
2023-05-04 22:51:47 +0530

    set the map method on transaction and display it
    add one button atlast for delete the method

--- tracker/src/component/TransactionHistory.js
+++ tracker/src/component/TransactionHistory.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
-export const TransactionHistory = () => {
+export const TransactionHistory = ({transactions,onDeletetransaction}) => {
   return (
-    <div>TransactionHistory</div>
+    <>
+    <h2 className='trh'>TransactionHistory</h2>
+    <ul className='transactions'>
+    {
+transactions.map((item)=><li key={item.id}>{item.name}   {item.amount}/-  {item.datetime} {item.showTime} <button className='close' onClick={()=>(onDeletetransaction(item.id))}>X</button></li>)
+    }
+     
+    </ul>
+    </>
   )
 }
