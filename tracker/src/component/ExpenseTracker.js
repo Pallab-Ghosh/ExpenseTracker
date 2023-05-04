0032e941112f42f68591fdbb842e8bf551224f2c
PALLAB GHOSH <gpallab405@gmail.com>
2023-05-04 22:56:53 +0530

    set the function for use the local storage

--- tracker/src/component/ExpenseTracker.js
+++ tracker/src/component/ExpenseTracker.js
@@ -6,6 +6,10 @@ export const ExpenseTracker = () => {
   const [income,setincome]=useState(0)
   const[expense,setexpense]=useState(0)
   const[transactions,settransactions]=useState(transactionData)
+  const saveState=()=>{
+    localStorage.setItem('data',JSON.stringify(transactions))
+ }
+
   return (
     <div>ExpenseTracker</div>
   )
